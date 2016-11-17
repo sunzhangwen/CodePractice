e2cb023fb8d3f4d060af7c47b90a50b384a23481
sunzhangwen <szw713@hotmail.com>
2016-11-17 17:41:01 +0800

    Test change

---
+++ HelloPython/src/com/demo/StringUtil.py
@@ -0,0 +1,9 @@
+'''
+Created on Nov 17, 2016
+
+@author: sunzhangwen
+'''
+#code=utf8
+
+def add(a, b):
+    return a + b
---
+++ HelloPython/src/com/demo/script.py
@@ -0,0 +1,43 @@
+#code=utf8
+'''
+Created on Sep 29, 2016
+
+@author: sunzhangwen
+'''
+#code=utf8
+import time
+import src.com.demo.StringUtil
+from src.com.demo import StringUtil
+
+"""
+Python get orientation
+"""
+print("Hello Python")
+
+name = "Bruce"
+print("Nice to see you %s, good morning!" %name)
+
+age = 27
+print("You are %d." %age)
+print("Hi %s, I know your %d" %(name,age))
+
+# When you don't know whick type it is
+pi = 3.1415926
+print("Pi is %r" %pi)
+name = 'Wayne'
+print("Hello %r" %name)
+
+# input data throught key board
+# n = input("Enter what you want:")
+# print("You enter %r, is that right?" %n)
+
+# import Model
+print(time.ctime())
+print("测试程序")
+time.sleep(1)
+print(time.ctime())
+
+print(StringUtil.add(60, 90))
+
+
+
